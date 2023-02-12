e4becba62570680c4d28e5ddc2a4575801900112
Farrux Ro'zmetov <farrukhruzmetov2002@gmail.com>
2023-02-12 17:07:07 +0500

    new project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WAD_CW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
